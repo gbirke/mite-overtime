c9a59c40d3855845881d27d805bddd0a09216d8c
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-10-15 15:31:28 +0200

    Make TimeAggregator locale-aware

--- js/time_aggregator.js
+++ js/time_aggregator.js
@@ -1,26 +1,30 @@
 var moment = require('moment');
 
-function TimeAggregator( rawEntries ) {
+function TimeAggregator( rawEntries, locale ) {
 	this._entries = rawEntries;
+	this.locale = typeof locale !== "undefined" ? locale : 'en';
 }
 
 TimeAggregator.prototype.getAggregatedData = function() {
 	var data = {
 		total: 0,
 		weeks: {}
-	}, 
+	},
 	entryCount = this._entries.length,
 	i, entry, day, firstDate, week;
 	if ( !entryCount ) {
 		return data;
 	}
 	firstDate = moment( this._entries[0].time_entry.date_at );
+	firstDate.locale( this.locale );
 	for ( i=0; i < entryCount; i++ ) {
 		entry = this._entries[i].time_entry;
 		day = moment( entry.date_at );
+		day.locale( this.locale );
 		if ( day.month() != firstDate.month() ) {
 			continue;
 		}
+		//moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 		week = day.week();
 		data.total += entry.minutes;
 		if ( !data.weeks.hasOwnProperty( week ) ) {
--- test/time_aggregator_test.js
+++ test/time_aggregator_test.js
@@ -32,17 +32,30 @@ describe( 'TimeAggregator', function() {
 	} );
 
 	it( 'adds total minutes for each week', function() {
-		// TODO Use German locale
 		var testData = [
-				{"time_entry":{"date_at":"2015-10-11","minutes":45}},
-				{"time_entry":{"date_at":"2015-10-11","minutes":60}},
-				{"time_entry":{"date_at":"2015-10-18","minutes":60}}
+				{"time_entry":{"date_at":"2015-10-13","minutes":45}},
+				{"time_entry":{"date_at":"2015-10-13","minutes":60}},
+				{"time_entry":{"date_at":"2015-10-20","minutes":60}}
 			],
 			aggregator = new TimeAggregator( testData ),
 			result = aggregator.getAggregatedData();
 		expect( result.weeks ).to.have.all.keys( ["42", "43" ] );
 		expect( result.weeks["42"].total ).to.equal( 105 );
 		expect( result.weeks["43"].total ).to.equal( 60 );
+	} );
+
+	it( 'can use different locales for week start dates', function() {
+		// 2015-10-11 is a Sunday (Week 41), 2015-10-15 is a Thursday (Week 42) 
+		var testData = [
+				{"time_entry":{"date_at":"2015-10-11","minutes":45}},
+				{"time_entry":{"date_at":"2015-10-11","minutes":60}},
+				{"time_entry":{"date_at":"2015-10-15","minutes":60}}
+			],
+			aggregator = new TimeAggregator( testData, 'de' ),
+			result = aggregator.getAggregatedData();
+		expect( result.weeks ).to.have.all.keys( ["41", "42" ] );
+		expect( result.weeks["41"].total ).to.equal( 105 );
+		expect( result.weeks["42"].total ).to.equal( 60 );
 	} );
 
 } );
