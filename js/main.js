31853943b720f173f4282edddbdc4a7ecd783994
Gabriel Birke <gb@birke-software.de>
2015-10-16 10:09:08 +0200

    Test main application
    Add HtmlRenderer with dummy data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// App goes here
+var HtmlRenderer = require( './html_renderer' ),
+	testData = {
+		total: 80,
+		weeks: [
+			{ total: 100, number: 42 },
+			{ total: -20, number: 43 }
+		]
+	},
+	renderer = new HtmlRenderer( testData );
+
+renderer.render();
+
