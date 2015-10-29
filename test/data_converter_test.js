89e4b9e2ce85292e20ec66a227ab309e6d332577
Gabriel Birke <gb@birke-software.de>
2015-10-29 22:48:48 +0100

    Test for timeDelta in DataConverter

--- test/data_converter_test.js
+++ test/data_converter_test.js
@@ -8,30 +8,34 @@ describe( 'DataConverter', function () {
 		var testData = {
 			year: 2015,
 			month: 9,
-			total: 140,
+			total: 2600,
+			timeDelta: 140,
 			weeks: {
 				42: {
-					total: 90,
+					total: 1050,
+					timeDelta: 90,
 					days: {
-						13: { total: 120 },
-						14: { total: -30 }
+						13: { total: 600, timeDelta: 120 },
+						14: { total: 450, timeDelta: -30 }
 					}
 				},
 				43: {
-					total: 50,
+					timeDelta: 50,
+					total: 530,
 					days: {
-						21: { total: 50 }
+						21: { total: 530, timeDelta: 50 }
 					}
 				}
 			}
 		};
 
-		it( 'copies total, year and month info', function () {
+		it( 'copies total, timeDelta, year and month info', function () {
 			var converter = new DataConverter(),
 				result = converter.convert( testData );
 			expect( result.length ).to.equal( 1 );
 			result = result[ 0 ];
-			expect( result.total ).to.equal( 140 );
+			expect( result.total ).to.equal( 2600 );
+			expect( result.timeDelta ).to.equal( 140 );
 			expect( result.year ).to.equal( 2015 );
 			expect( result.month ).to.equal( 9 );
 		} );
@@ -42,9 +46,11 @@ describe( 'DataConverter', function () {
 			result = result[ 0 ];
 			expect( result.weeks.length ).to.equal( 2 );
 			expect( result.weeks[ 0 ].week ).to.equal( '42' );
-			expect( result.weeks[ 0 ].total ).to.equal( 90 );
+			expect( result.weeks[ 0 ].total ).to.equal( 1050 );
+			expect( result.weeks[ 0 ].timeDelta ).to.equal( 90 );
 			expect( result.weeks[ 1 ].week ).to.equal( '43' );
-			expect( result.weeks[ 1 ].total ).to.equal( 50 );
+			expect( result.weeks[ 1 ].total ).to.equal( 530 );
+			expect( result.weeks[ 1 ].timeDelta ).to.equal( 50 );
 		} );
 
 		it( 'converts days into arrays', function () {
@@ -55,9 +61,13 @@ describe( 'DataConverter', function () {
 			expect( firstWeek.days.length ).to.equal( 2 );
 			expect( secondWeek.days.length ).to.equal( 1 );
 
-			expect( firstWeek.days[ 0 ].total ).to.equal( 120 );
-			expect( firstWeek.days[ 1 ].total ).to.equal( -30 );
-			expect( secondWeek.days[ 0 ].total ).to.equal( 50 );
+			expect( firstWeek.days[ 0 ].total ).to.equal( 600 );
+			expect( firstWeek.days[ 1 ].total ).to.equal( 450 );
+			expect( secondWeek.days[ 0 ].total ).to.equal( 530 );
+
+			expect( firstWeek.days[ 0 ].timeDelta ).to.equal( 120 );
+			expect( firstWeek.days[ 1 ].timeDelta ).to.equal( -30 );
+			expect( secondWeek.days[ 0 ].timeDelta ).to.equal( 50 );
 
 			expect( firstWeek.days[ 0 ].day ).to.equal( '13' );
 			expect( firstWeek.days[ 1 ].day ).to.equal( '14' );
