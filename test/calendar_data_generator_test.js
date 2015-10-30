8ae3eac575af7b82fd0636e7d9174aa38f778236
Gabriel Birke <gb@birke-software.de>
2015-10-30 08:44:27 +0100

    Add CalendarDataGenerator
    It generates a data structure for rendering a whole month, including the
    days.
    It will replace DataConverter in the future.

---
+++ test/calendar_data_generator_test.js
@@ -0,0 +1,50 @@
+var expect = require( 'chai' ).expect,
+	moment = require( 'moment' ),
+	DayTypes = require( '../js/day_types' ),
+	WorktimeCalculator = require( '../js/worktime_calculator' ),
+	CalendarDataGenerator = require( '../js/calendar_data_generator' );
+
+describe( 'CalendarDataGenerator', function () {
+
+	describe( '#generateData, English locale', function () {
+
+		var worktime = new WorktimeCalculator( [ 1, 2, 3, 4, 5 ] ),
+			generator = new CalendarDataGenerator( worktime ),
+			year = 2015,
+			month = 9;
+
+		it( 'generates year and month info', function () {
+			var result;
+			moment.locale( 'en' );
+			result = generator.generateData( year, month );
+			expect( result.year ).to.equal( 2015 );
+			expect( result.month ).to.equal( 9 );
+		} );
+
+		it( 'generates weeks info', function () {
+			var result;
+			moment.locale( 'en' );
+			result = generator.generateData( year, month );
+			expect( result.weeks ).to.have.all.keys( '40', '41', '42', '43', '44' );
+		} );
+
+		it( 'generates day info', function () {
+			var result;
+			moment.locale( 'en' );
+			result = generator.generateData( year, month );
+			expect( result.weeks[40].days ).to.have.all.keys( '1', '2', '3' );
+			expect( result.weeks[41].days ).to.have.all.keys( '4', '5', '6', '7', '8', '9', '10' );
+		} );
+
+		it( 'includes workday info in day info', function () {
+			var result;
+			moment.locale( 'en' );
+			result = generator.generateData( year, month );
+			expect( result.weeks[40].days[1].dayType ).to.equal( DayTypes.WORKDAY );
+			expect( result.weeks[40].days[2].dayType ).to.equal( DayTypes.WORKDAY );
+			expect( result.weeks[40].days[3].dayType ).to.equal( DayTypes.HOLIDAY );
+			expect( result.weeks[41].days[4].dayType ).to.equal( DayTypes.HOLIDAY );
+		} );
+
+	} );
+} );
