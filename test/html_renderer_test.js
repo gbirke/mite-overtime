121a1301e86bd107c7a8af81b2222dfb21dc7b0e
Gabriel Birke <gb@birke-software.de>
2015-11-08 15:46:59 +0100

    Add tests for labels in HTML Renderer
    The renderer must show the month name and week numbers.

--- test/html_renderer_test.js
+++ test/html_renderer_test.js
@@ -5,9 +5,11 @@ describe( 'HtmlRenderer', function () {
 
 	var testData = {
 		timeDelta: 80,
+		month: 9,
+		year: 2015,
 		weeks: {
-			42: { timeDelta: 100, number: 42 },
-			43: { timeDelta: -20, number: 43 }
+			42: { timeDelta: 100, week: 42 },
+			43: { timeDelta: -20, week: 43 }
 		}
 	};
 
@@ -22,6 +24,15 @@ describe( 'HtmlRenderer', function () {
 			done();
 		} );
 	} );
+
+	it( 'renders the month name and year', function () {
+		var total;
+		renderer.render( testData );
+		total = displayContainer.select( 'h1' );
+		expect( total.size() ).to.equal( 1 );
+		expect( total.text() ).to.equal( 'Total for November 2015' );
+	} );
+
 
 	it( 'renders the overall time delta', function () {
 		var total;
@@ -39,6 +50,16 @@ describe( 'HtmlRenderer', function () {
 		expect( displayContainer.select( '.week:nth-child(1) .total' ).text() ).to.equal( '1:40 overtime' );
 		expect( displayContainer.select( '.week:nth-child(2) .total' ).text() ).to.equal( '0:20 missing' );
 	} );
+
+	it( 'renders the week number', function () {
+		var weeks;
+		renderer.render( testData );
+		weeks = displayContainer.selectAll( '.week h2' );
+		expect( weeks.size() ).to.equal( 2 );
+		expect( displayContainer.select( '.week:nth-child(1) h2' ).text() ).to.equal( 'Week 42' );
+		expect( displayContainer.select( '.week:nth-child(2) h2' ).text() ).to.equal( 'Week 43' );
+	} );
+
 
 	after( function () {
 		benv.teardown();
