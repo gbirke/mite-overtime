79ea14242304278e26deac96ecf5868c244c1858
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-11-14 10:22:10 +0100

    Refactor EntriesStore
    Module now exports only factory function that needs all dependencies

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 var $ = jQuery = require( 'jQuery' ),
 	SettingsController = require( './settings_controller' ),
-	entriesStore = require( './stores/entries' ),
+	ServerConnector = require( './server_connector' ),
+	CalendarDataGenerator = require( './calendar_data_generator' ),
+	WorkTimeCalculator = require( './worktime_calculator' )
+	OvertimeCalculator = require( './weekly_overtime_calculator' ),
+	EntriesStore = require( './stores/entries' ),
 	EntryView = require('./views/entry_view'),
 	Bootstrap = require( 'bootstrap' );
 
 $( function () {
+	var serverConnector = new ServerConnector( 'http://localhost:8080/time_entries.json', XMLHttpRequest ),
+		worktimeCalculator = new WorkTimeCalculator( [ 1, 2, 3, 4, 5 ] ),
+		calendarDataGenerator = new CalendarDataGenerator( worktimeCalculator ),
+		overtimeCalculator = new OvertimeCalculator( worktimeCalculator, 40 ),
+		entriesStore = EntriesStore.create( serverConnector, overtimeCalculator, calendarDataGenerator );
+
 	EntryView.createAndInit( entriesStore );
 	new SettingsController();
 } );
--- js/stores/entries.js
+++ js/stores/entries.js
@@ -1,49 +1,36 @@
 var serverActions = require( '../actions/server' ),
 	settingsStore = require( './settings' ),
 	Reflux = require( 'reflux-core' ),
-	TimeAggregator = require( '../time_aggregator' ),
-	CalendarDataGenerator = require( '../calendar_data_generator' ),
-	WorkTimeCalculator = require( '../worktime_calculator' ),
-	OvertimeCalculator = require( '../weekly_overtime_calculator' ),
-	ServerConnector = require( '../server_connector' ),
-	entriesStore = Reflux.createStore( {
-		init: function() {
-			this.listenTo( settingsStore, this.onSettingsChanged );
-			this.listenToMany( serverActions );
-			this.entries = {};
-			// TODO research how to inject dependent objects instead of creating them
-			this.worktimeCalc = new WorkTimeCalculator( [ 1, 2, 3, 4, 5 ] ); 
-			this.overtimeCalculator = new OvertimeCalculator( this.worktimeCalc );
-			// TODO move this to factory
-			this.serverConnector = new ServerConnector( 'http://localhost:8080/time_entries.json', XMLHttpRequest );
-		},
-		onSettingsChanged: function( settings ) {
-			var calendarDataGenerator = new CalendarDataGenerator( this.worktimeCalc );
-			this.calendarData = calendarDataGenerator.generateData( settings.year, settings.month );
-			this.hoursPerWeek = settings.hoursPerWeek;
-			serverActions.load( { apiKey: settings.apiKey, account: settings.account } );
-		},
-		onLoad: function( credentials ) {
-			if ( !this.serverConnector ) {
-				throw Error( 'Uninitialized serverConnector' );
-			}
-			this.serverConnector.getData( serverActions.load.completed, serverActions.load.failed );
-		},
-		onLoadCompleted: function (result) {
-			var agggregator = new TimeAggregator( result ),
-				overtimeData = this.overtimeCalculator.getOvertime( agggregator.getAggregatedData(), this.hoursPerWeek );
-			console.log("success!");
-			console.log(this.calendarData);
-			this.trigger( { calendarData: this.calendarData, overtimeData: overtimeData } );
-		},
-		onLoadFailed: function (result) {
-			console.log("fail!");
-			console.log(result);
-		},
-		serverConnector: null,
-		calendarData: {},
-		hoursPerWeek: 40
+	TimeAggregator = require( '../time_aggregator' );
 
-	} );
-
-module.exports = entriesStore;
+module.exports = {
+	create: function ( serverConnector, overtimeCalculator, calendarDataGenerator ) {
+		return Reflux.createStore( {
+			init: function() {
+				this.listenTo( settingsStore, this.onSettingsChanged );
+				this.listenToMany( serverActions );
+				this.entries = {};
+			},
+			onSettingsChanged: function( settings ) {
+				this.calendarData = calendarDataGenerator.generateData( settings.year, settings.month );
+				overtimeCalculator.hoursPerWeek = settings.hoursPerWeek;
+				serverActions.load( { apiKey: settings.apiKey, account: settings.account } );
+			},
+			onLoad: function( credentials ) {
+				serverConnector.getData( serverActions.load.completed, serverActions.load.failed );
+			},
+			onLoadCompleted: function ( result ) {
+				var agggregator = new TimeAggregator( result );
+				this.entries = overtimeCalculator.getOvertime( agggregator.getAggregatedData() );
+				console.log("success!");
+				console.log(this.calendarData);
+				this.trigger( { calendarData: this.calendarData, overtimeData: this.entries } );
+			},
+			onLoadFailed: function ( result ) {
+				console.log("fail!");
+				console.log(result);
+			},
+			calendarData: {},
+		} );
+	}
+}
