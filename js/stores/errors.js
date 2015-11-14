f865d0199e92a5efbf110a8b5506527f86a338e4
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-11-14 20:23:57 +0100

    Add ErrorView and ErrorStore
    Move error handling of failing server requests to its own view and
    store.

---
+++ js/stores/errors.js
@@ -0,0 +1,16 @@
+var Reflux = require( 'reflux-core' ),
+	serverActions = require( '../actions/server' ),
+	errorStore = Reflux.createStore( {
+		init: function() {
+			this.listenTo( serverActions.load.failed, this.onLoadFailed );
+		},
+		onLoadFailed: function( errorData, xhr ) {
+			this.errorMessage = errorData.error;
+			this.lastXHR = xhr;
+			this.trigger();
+		},
+		errorMessage: '',
+		lastXHR: null
+	} );
+
+module.exports = errorStore;
