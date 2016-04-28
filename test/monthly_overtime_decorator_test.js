ef1f3c369d1797c494ccffba84bfe854a223e5a1
Gabriel Birke <gabriel.birke@gmail.com>
2016-04-29 00:23:26 +0200

    Add monthly overtime decorator
    Will be used in place of WeeklyOvertimeCalculator

---
+++ test/monthly_overtime_decorator_test.js
@@ -0,0 +1,31 @@
+var expect = require( 'chai' ).expect,
+    MonthlyOvertimeDecorator = require( '../js/monthly_overtime_decorator' );
+
+function createWeek( weekNumber, minutesWorked ) {
+    return {
+        weekNumber: weekNumber,
+        getMinutesWorked: sinon.stub().returns( minutesWorked )
+    };
+}
+
+describe( 'MonthlyOvertimeDecorator', function () {
+
+    it( 'sums up the time delta of the contained weeks', function () {
+        var firstMonth = {
+                weeks: {
+                    40: { timeDelta: 0 },
+                    41: { timeDelta: 50 },
+                    42: { timeDelta: -8 }
+                }
+            },
+            months = {
+                9: firstMonth
+            },
+            decorator = MonthlyOvertimeDecorator.createMonthlyOvertimeDecorator();
+        decorator.addOvertimeToEntries( months );
+
+        expect( firstMonth ).to.have.property( 'timeDelta' );
+        expect( firstMonth.timeDelta ).to.equal( 42 );
+
+    } );
+} );
