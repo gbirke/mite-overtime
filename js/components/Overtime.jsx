514fb8ebe23a7f069289a83e90028774b11da541
Gabriel Birke <gabriel.birke@gmail.com>
2016-09-02 20:13:49 +0200

    Use Boostrap components
    Use Boostrap components in Overtime display. Only placeholders for main
    content and pagination.

--- js/components/Overtime.jsx
+++ js/components/Overtime.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 
 export default function() {
-	return ( <div>You'll see your overtime data soon.</div> );
+	return (
+		<Row>
+			<Col md={1}/>
+			<Col md={10} ><div>You'll see your overtime data soon.</div></Col>
+			<Col md={1}/>
+		</Row>
+	);
 };
