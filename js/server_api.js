833c7b1068499cc0c5811aae51023833da0b4fd6
Gabriel Birke <gabriel.birke@gmail.com>
2016-09-08 22:06:13 +0200

    Refactor Server API
    Move XHR wrapped in Promise to its own function. Use `then` method to
    decode JSON. JSOn erors wil automatically reject the promise.

--- js/server_api.js
+++ js/server_api.js
@@ -3,64 +3,54 @@ const moment = require( 'moment' );
 export const ERROR_SERVER = 500;
 export const ERROR_CREDENTIALS = 403;
 
+function doRequest( url, account, apiKey, method='GET' ) {
+	return new Promise( ( resolve, reject ) => {
+		const xhr = new XMLHttpRequest();
+		xhr.addEventListener( 'load', function () {
+			if ( this.status !== 200 ) {
+				// TODO Use Error subclass instead
+				reject( ERROR_CREDENTIALS );
+			} else {
+				resolve( this.responseText );
+			}
+		} );
+		xhr.addEventListener( 'error', function () {
+			// TODO Use Error subclass instead
+			reject( ERROR_SERVER );
+		} );
+		xhr.open( method, url, true );
+		xhr.setRequestHeader( 'X-MiteAccount', account );
+		xhr.setRequestHeader( 'X-MiteApiKey', apiKey );
+		xhr.send();
+	} );
+}
+
+
 export default class ServerApi {
 	constructor( baseUrl ) {
 		this.baseUrl = baseUrl;
 	}
 
 	checkCredentials( account, apiKey ) {
-		return new Promise( ( resolve, reject ) => {
-			const xhr = new XMLHttpRequest();
-			xhr.addEventListener( 'load', function () {
-				if ( this.status !== 200 ) {
-					// TODO Use Error subclass instead
-					reject( ERROR_CREDENTIALS );
-				} else {
-					resolve( true );
-				}
-			} );
-			xhr.addEventListener( 'error', function () {
-				// TODO Use Error subclass instead
-				reject( ERROR_SERVER );
-			} );
-			xhr.open( 'HEAD', this.baseUrl + 'myself.json', true );
-			xhr.setRequestHeader( 'X-MiteAccount', account );
-			xhr.setRequestHeader( 'X-MiteApiKey', apiKey );
-			xhr.send();
-		} );
+		return doRequest( this.baseUrl + 'myself.json', account, apiKey, 'HEAD' ).then( () => ( true ) )
 	}
 
 	loadEntries( account, apiKey, year, month ) {
-		return new Promise( ( resolve, reject ) => {
-			const xhr = new XMLHttpRequest();
 			let from = moment( [ year, month ] );
 			let to = moment( from ).add( 1, 'month' ).subtract( 1, 'day' );
 			let params = '?from=' + from.format( 'YYYY-MM-DD' ) + '&to=' + to.format( 'YYYY-MM-DD' );
-			xhr.addEventListener( 'load', function () {
-				let entries = [];
-				if ( this.status !== 200 ) {
-					// TODO Use Error subclass instead
-					reject( ERROR_CREDENTIALS );
-				} else {
-					try {
-						entries = JSON.parse( this.responseText )
-					} catch ( e ) {
-						// TODO Use Error subclass instead
-						reject( ERROR_SERVER );
-					}
-					resolve( entries );
-				}
-			} );
-			xhr.addEventListener( 'error', function () {
-				// TODO Use Error subclass instead
-				reject( ERROR_SERVER );
-			} );
+			return doRequest( this.baseUrl + 'time_entries.json' + params, account, apiKey ).then(
+				( response ) => ( JSON.parse( response ) )
+			);
+	}
 
-			xhr.open( 'GET', this.baseUrl + 'time_entries.json' + params, true );
-			xhr.setRequestHeader( 'X-MiteAccount', account );
-			xhr.setRequestHeader( 'X-MiteApiKey', apiKey );
-			xhr.send();
-		} );
+	getYearlyTime( account, apiKey, year ) {
+		let from = moment( [ year, month ] );
+		let to = moment( from ).add( 1, 'month' ).subtract( 1, 'day' );
+		let params = '?from=' + from.format( 'YYYY-MM-DD' ) + '&to=' + to.format( 'YYYY-MM-DD' );
+		return doRequest( this.baseUrl + 'time_entries.json' + params, account, apiKey ).then(
+			( response ) => ( JSON.parse( response ) )
+		);
 	}
 
 }
